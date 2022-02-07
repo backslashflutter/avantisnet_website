9eb1ae60f93e964b26e8b5901602d1671f35db9c
BackSlash Flutter <backslash.flutter@gmail.com>
2022-02-07 10:41:12 -0800

    main

--- src/components/navbar/Navlinks.js
+++ src/components/navbar/Navlinks.js
@@ -15,8 +15,8 @@ const NavLinks = () => {
             <HashLink className='px-4 ml-0 mr-0 font-semibold duration-100 md:ml-12 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600' smooth to="/#services">
                 Services
             </HashLink>
-            <HashLink className='px-4 ml-0 mr-0 font-semibold duration-100 md:ml-12 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600' smooth to="/#testimonials">
-               Testimonials
+            <HashLink className='px-4 ml-0 mr-0 font-semibold duration-100 md:ml-12 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600' smooth to="/#portfolio">
+               Portfolio
             </HashLink>
 
             <HashLink className='bg-blue-800 px-3 ml-0 mr-0 font-medium duration-100 md:ml-12 md:mr-3 text-white text-base lg:mr-8 transition-color hover:text-white rounded-xl  w-full  py-3 my-4 text-lg shadow-xl rounded-xl sm:w-auto sm:mb-0' smooth to="/contact">
