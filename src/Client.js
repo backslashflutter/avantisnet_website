2e714ee96621c2b4ca7109a044fa5565a3f08986
BackSlash Flutter <75381412+backslashflutter@users.noreply.github.com>
2022-06-09 22:48:11 +0530

    Update Client.js

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,6 @@
 import * as contentful from "contentful";
 
 export const client = contentful.createClient({
-  space: "tud2s89vbf39",
-  accessToken: "UucfWXV7kkQ9SL3-R2P1bPlRqLQggtu81dLfL_ZpTeA",
+  space: "SPACE ID",
+  accessToken: "ACCESS TOKEN",
 });
